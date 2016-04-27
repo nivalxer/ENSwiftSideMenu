a1f01d50a1937aa0f24357debbce81084ba7ae62
nivalxer <nivalxer@users.noreply.github.com>
2016-04-27 16:15:51 +0800

    diy

--- Library/ENSideMenu.swift
+++ Library/ENSideMenu.swift
@@ -151,7 +151,9 @@ public class ENSideMenu : NSObject, UIGestureRecognizerDelegate {
     public var allowRightSwipe : Bool = true
     public var allowPanGesture : Bool = true
     private var panRecognizer : UIPanGestureRecognizer?
-    
+	
+	private var outterView: UIView = UIView()
+	
     /**
     Initializes an instance of a `ENSideMenu` object.
     
@@ -192,7 +194,15 @@ public class ENSideMenu : NSObject, UIGestureRecognizerDelegate {
             sideMenuContainerView.addGestureRecognizer(rightSwipeGestureRecognizer)
             sourceView.addGestureRecognizer(leftSwipeGestureRecognizer)
         }
-        
+		
+		outterView = UIView(frame: CGRectMake(sideMenuContainerView.frame.width, 0,
+			sourceView.frame.width - sideMenuContainerView.frame.width,
+			sourceView.frame.height))
+		outterView.backgroundColor = UIColor.clearColor()
+		let tapRecognizer = UITapGestureRecognizer(target: self, action: "hideSideMenu")
+		outterView.addGestureRecognizer(tapRecognizer)
+		outterView.userInteractionEnabled = false
+		sourceView.addSubview(outterView)
     }
     /**
     Initializes an instance of a `ENSideMenu` object.
